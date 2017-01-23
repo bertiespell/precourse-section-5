21ef5637730e79f1baa5945fc73795d3a23f1887
Bertie Spell <berniesnell@gmail.com>
2017-01-23 18:47:20 +0000

    all the sites functionality is now working, just need to add content and style

--- js/main.js
+++ js/main.js
@@ -1,14 +1,15 @@
 var counter = 0;
 $(document).ready(function(){
   $(".correct").click(function(){
-    console.log("correct!");
-    $(this).removeClass("correct");
-      counter += 1;
-    $(this).addClass("correctAnswer");
+    counter += 1;
     document.getElementById("score").innerHTML = counter;
+    $(this).addClass("correctAnswer");
+    $(this).unbind();
+    $(this).siblings().unbind();
   });
   $(".wrong").click(function(){
-    console.log("wrong!");
     $(this).addClass("wrongAnswer");
+    $(this).unbind();
+    $(this).siblings().unbind();
   });
 });
