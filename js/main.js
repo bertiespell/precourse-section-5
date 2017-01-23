9b38bab001975d05b07a753dc821a683a7df7bb8
Bertie Spell <berniesnell@gmail.com>
2017-01-23 14:38:26 +0000

    the counter div is now working

--- js/main.js
+++ js/main.js
@@ -1,8 +1,14 @@
+var counter = 0;
 $(document).ready(function(){
-  $("#correct").click(function(){
+  $(".correct").click(function(){
     console.log("correct!");
+    $(this).removeClass("correct");
+      counter += 1;
+    $(this).addClass("correctAnswer");
+    document.getElementById("score").innerHTML = counter;
   });
-  $("#wrong").click(function(){
+  $(".wrong").click(function(){
     console.log("wrong!");
+    $(this).addClass("wrongAnswer");
   });
 });
