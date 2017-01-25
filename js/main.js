994741e6100478322e8028cb43bf1f4a385d0dd6
Bertie Spell <berniesnell@gmail.com>
2017-01-25 15:45:57 +0000

    completed

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
 var counter = 0;
 $(document).ready(function(){
+  $("#scrollBar").animate({left: '200px'}, "slow");
   $(".correct").click(function(){
     counter += 1;
     document.getElementById("score").innerHTML = counter;
